952de21b2c7e11d76a0fcbb7b74b6f1697e50361
Tetsushi Haresaku <haresaku1024@gmail.com>
2026-04-02 07:56:02 +0900

    refactor: simplify App.tsx to 2-layer render (ThreeCanvas + SceneManager)

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,28 @@
 import { useEffect } from 'react'
 import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from '@/firebase'
-import { useScreenStore } from '@/states/screenStore'
-import { SCREEN, CONFIG } from '@/constants'
-import { Layout } from '@/components/Layout'
+import { useSceneStore } from '@/scene-manager/sceneStore'
+import { CONFIG } from '@/constants'
+import { ThreeCanvas } from '@/components/ThreeCanvas'
+import { SceneManager } from '@/scene-manager/SceneManager'
 import { DebugMenu } from '@/components/DebugMenu'
-import { StartScreen } from '@/components/screens/StartScreen'
-import { MainScreen } from '@/components/screens/MainScreen'
-import { EndingScreen } from '@/components/screens/EndingScreen'
-import { SignInScreen } from '@/components/screens/SignInScreen'
-import { AssetUpdater } from '@/components/modules/AssetUpdater'
 
 const App = () => {
-  const { screenState, setScreen } = useScreenStore()
-  const { screen } = screenState
+  const navigate = useSceneStore(s => s.navigate)
 
-  // Redirect to SIGN_IN if Firebase Auth session expires
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, user => {
-      if (!user) setScreen({ screen: SCREEN.SIGN_IN })
+      if (!user) navigate('sign-in')
     })
     return unsubscribe
-  }, [setScreen])
+  }, [navigate])
 
   return (
-    <Layout>
-      {screen === SCREEN.SIGN_IN       && <SignInScreen />}
-      {screen === SCREEN.ASSET_UPDATE  && <AssetUpdater />}
-      {screen === SCREEN.START_SCREEN  && <StartScreen />}
-      {screen === SCREEN.MAIN_SCREEN   && <MainScreen />}
-      {screen === SCREEN.ENDING_SCREEN && <EndingScreen />}
+    <div className="relative h-[100svh] max-h-[100svh] overflow-hidden select-none">
+      <ThreeCanvas />
+      <SceneManager />
       {CONFIG.DEBUG && <DebugMenu />}
-    </Layout>
+    </div>
   )
 }
 
